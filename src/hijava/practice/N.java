876bcc41fc0cf3f6c9a8a3aae5d65193873541da
indiflex <indiflex1@gmail.com>
2019-01-13 12:45:37 +0900

    첫번째 커밋

---
+++ src/hijava/practice/N.java
@@ -0,0 +1,30 @@
+package hijava.practice;
+
+public class N {
+	int x = 10;
+	int y = 20;
+	
+	public void swap() {
+		int t = x;
+		x = y;
+		y = t;
+	}
+	
+	public void swap2() {
+		int x1 = x;
+		int y1 = y;
+		x = y1;
+		y = x1;
+	}
+
+	public static void main(String[] args) {
+		N n = new N();
+		System.out.println("x=" + n.x);
+		System.out.println("y=" + n.y);
+		n.swap2();
+		System.out.println("---------------------");
+		System.out.println("x=" + n.x);
+		System.out.println("y=" + n.y);
+	}
+
+}
---
+++ src/hijava/practice/Str.java
@@ -0,0 +1,17 @@
+package hijava.practice;
+
+public class Str {
+	public static void main(String[] args) {
+		char c = '한';		// cf. char c = 65;    or  char c = '\uD55C';
+		byte b = 'B';
+		System.out.println(c);
+		System.out.println( (int)c );    // cf. (char)c  or   (char)b
+		System.out.println(b);
+		
+		String str = "AB";
+		System.out.println(str);
+		System.out.println("AB".getBytes().length);
+		System.out.println("AB한".getBytes().length);
+		System.out.println("한".getBytes().length);
+	}
+}
