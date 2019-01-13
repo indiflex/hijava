42218f515aa19f68a7647ed575f4b6fdf9d908a2
indiflex <indiflex1@gmail.com>
2019-01-13 13:34:23 +0900

    1111

--- src/hijava/practice/Str.java
+++ src/hijava/practice/Str.java
@@ -2,7 +2,7 @@ package hijava.practice;
 
 public class Str {
 	public static void main(String[] args) {
-		char c = '한';		// cf. char c = 65;    or  char c = '\uD55C';
+		char c = '뷐';  		//   cf. char c = 65;    or  char c = '\uD55C';
 		byte b = 'B';
 		System.out.println(c);
 		System.out.println( (int)c );    // cf. (char)c  or   (char)b
