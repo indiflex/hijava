cafa5bf77f0eb1c00e57299eeb59af4f5e83abea
indiflex <indiflex1@gmail.com>
2019-01-26 13:20:29 +0900

    array copy

--- src/hijava/practice/Arr.java
+++ src/hijava/practice/Arr.java
@@ -4,7 +4,57 @@ public class Arr {
 
 	public static void main(String[] args) {
 //		ex1();
-		ex2();
+//		ex2();
+//		ex3();
+//		ex4();
+		ex5();
+	}
+
+	private static void ex5() {
+		int[] arr1 = {10, 20, 30, 40, 50};
+		int[] arr2 = { 1, 2, 3, 4, 5 };
+		System.arraycopy(arr1, 0, arr2, 2, 3);
+		
+		for (int i : arr2) {
+			System.out.println(i);
+		}
+	}
+
+	private static void ex4() {
+		Man[] people = new Man[9];
+		int len = people.length;
+		for (int i = 0; i < len; i++) {
+			people[i] = new Man("김" + (i+1) + "수");
+		}
+		
+		Man[] people2 = new Man[len];
+		for (int i = 0; i < len; i++) {
+//			people2[i] = people[i]; //shallow copy
+			people2[i] = new Man(people[i].getName()); //deep copy (clone)
+		}
+		
+		people[0].setName("김1111111");
+		
+		System.out.println("1=" + people[0].getName());
+		System.out.println("2=" + people2[0].getName());
+		
+//		for (Man man : people2) {
+//			System.out.println(man);
+//		}
+	}
+
+	private static void ex3() {
+		Man[] people = new Man[9];
+		int len = people.length;
+		for (int i = 0; i < len; i++) {
+//			Man m = new Man("김" + (i+1) + "수");
+//			people[i] = m;
+			people[i] = new Man("김" + (i+1) + "수");
+		}
+		
+		for (Man man : people) {
+			System.out.println(man);
+		}
 	}
 
 	private static void ex2() {
